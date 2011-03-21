b4f06c0bf7abd960725bfde4cd7b0e5fe2817227
Tim Caswell <tim.caswell@palm.com>
2011-03-21 10:14:12 -0700

    More fixes
    Change-Id: I368b9e3db00214ea937520401a83c06fcb6510af

--- clock.js
+++ clock.js
@@ -11,8 +11,19 @@ function tick() {
   deg = seconds * 6;
   second.style.webkitTransform = "rotate(" + deg + "deg)";
 }
+
+function resize() {
+  return;
+  var width = window.innerWidth,
+      height = window.innerHeight;
+  var size = (width > height) ? height : width;
+  frame.style.webkitTransform = "scale(" + (size / 500) + ")";
+}
+var frame;
+
 window.addEventListener('load', function () {
   var div = document.getElementById('numbers');
+  frame = document.getElementById('back');
   for (var i = 1; i <= 12; i++) {
     var angle = (i * 30) / 180 * Math.PI;
     var h = 170;
@@ -30,8 +41,13 @@ window.addEventListener('load', function () {
   hour = document.getElementById("hour-hand");
   minute = document.getElementById("minute-hand");
   second = document.getElementById("second-hand");
+
   tick();
   setInterval(tick, 1000);
+
+  resize();
+  window.addEventListener('resize', resize);
+
   if (typeof PalmSystem !== 'undefined') {
     PalmSystem.stageReady();
     PalmSystem.enableFullScreenMode(true);
